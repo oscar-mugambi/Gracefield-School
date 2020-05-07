61f42e93ca594fc2b91ab527ce1dc28ab0e4a65b
Oscar Mugambi <oscarmugambi@gmail.com>
2020-05-07 22:47:45 +0300

    responsive layout

---
+++ Pages/Contact/menu.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".mobile-links");
+const shade = document.querySelector(".shade");
+const navItems = document.querySelectorAll(".nav-item");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener("click", console.log("clicked"));
+
+function toggleMenu() {
+  console.log("clicked");
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBtn.classList.add("show");
+    shade.classList.add("dim");
+    navItems.forEach((item) => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBtn.classList.remove("show");
+    shade.classList.remove("dim");
+    navItems.forEach((item) => item.classList.remove("show"));
+    showMenu = false;
+  }
+}
