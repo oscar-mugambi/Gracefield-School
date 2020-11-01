d94eb6d5a3b690360b79b65c4405600baaca929f
Oscar Mugambi <oscarmugambi@gmail.com>
2020-11-01 13:10:38 +0300

    Complete

--- js/menu.js
+++ js/menu.js
@@ -36,3 +36,8 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+var scale = "scale(1)";
+document.body.style.webkitTransform = scale; // Chrome, Opera, Safari
+document.body.style.msTransform = scale; // IE 9
+document.body.style.transform = scale; // General
